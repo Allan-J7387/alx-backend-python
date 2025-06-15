e1368ee18a18ccb64657e2c1c2c9d45c744d0cb5
Jayallan Langat <langatjayallan@gmail.com>
2025-06-15 14:44:42 +0300

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,20 +1,17 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from .models import Message, MessageHistory
+from django.contrib.auth.models import User
+from .models import Message, Notification, MessageHistory
 
-@receiver(pre_save, sender=Message)
-def log_message_edit(sender, instance, **kwargs):
-    if not instance.pk:
-        return  # New message, not an edit
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # Delete messages sent or received
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
 
-    try:
-        original = Message.objects.get(pk=instance.pk)
-    except Message.DoesNotExist:
-        return
+    # Delete notifications
+    Notification.objects.filter(user=instance).delete()
 
-    if original.content != instance.content:
-        MessageHistory.objects.create(
-            message=original,
-            old_content=original.content
-        )
-        instance.edited = True
+    # Delete message histories where message belonged to deleted user
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    MessageHistory.objects.filter(message__receiver=instance).delete()
